2864f1412ea903433435f4c95e8af49f23e0590f
madol-abraham <m.madol@alustudent.com>
2024-09-13 06:31:02 -0700

    6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for concatenating two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Parameters:
+    arr1 (list): The first array.
+    arr2 (list): The second array.
+
+    Returns:
+    A new list containing the elements of arr1 followed by arr2.
+    """
+    return arr1 + arr2
